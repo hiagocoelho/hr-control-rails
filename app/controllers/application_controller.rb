02a236b17259e16178ef8da77400ae4fb8b96e6f
Hiago Coelho <b8coelho@gmail.com>
2023-09-04 14:56:43 -0300

    feat: handles a request with missing parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
+
+  def handle_parameter_missing(exception)
+    render json: { error: exception.message }, status: :bad_request
+  end
 end
